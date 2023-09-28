552840e0af00f7d519348948281b608ba066ed35
BrunoVinicius28 <125374063+BrunoVinicius28@users.noreply.github.com>
2023-09-28 16:21:08 -0300

    Add files via upload

---
+++ Relatório 4/Questão 2.asm
@@ -0,0 +1,13 @@
+# Questão 2
+.text
+   li $t1, 324
+   li $t2, 100
+   li $t3, 2018
+   li $t4, 1970
+   li $t5, 33
+   li $t6, 66
+
+   # Lógica AND
+   and $s0, $t1, $t2
+   and $s1, $t3, $t4
+   and $s2, $t5, $t6
