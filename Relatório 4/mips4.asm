5d04ea92393193d57875dc03f28c1c779d5452b3
BrunoVinicius28 <125374063+BrunoVinicius28@users.noreply.github.com>
2023-09-28 16:18:49 -0300

    Add files via upload

---
+++ Relatório 4/mips4.asm
@@ -0,0 +1,13 @@
+# Questão 4
+.text
+   li $t1, 324
+   li $t2, 100
+   li $t3, 2018
+   li $t4, 1970
+   li $t5, 33
+   li $t6, 66
+
+   # Lógica NOR
+   nor $s0, $t1, $t2
+   nor $s1, $t3, $t4
+   nor $s2, $t5, $t6
